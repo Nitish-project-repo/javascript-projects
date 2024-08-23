dda3d7e714f5becdd01689672b47d846703b85c7
Nitish-project-repo <nitish.projects.repo@gmail.com>
2024-08-23 06:16:19 +0000

    adding notes on 01_variables and 02_dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+/**
+ *  Javascript has let , const  and var to declare variables in the code .
+ *  let -> for changeable-value storing variables
+ *  const -> for unchangeable-value storing variables ; once declared will not change in later part of the program
+ *  var -> older way of declaring changeable-value storing variables.
+ */
+
+
+let accountHolderEmail = "Dim@hi.com";
+const accountPassword = "Stella@123";
+var accoutNumber = 12345;
+accountHolderCity = "Jamshedpur";
+let isAccountActive = true;
+
+// console.table([accoutNumber,accountHolderEmail,accountPassword,accountHolderCity,isAccountActive]);
+
+
+// Now changing value for each variables decalred
+
+accountHolderEmail = "Tim@hi.com";
+accountPassword = "Stupa@123";     // TypeError: Assignment to constant variable. Thus, changing a const variable will give this error
+accountNumber = 5678;
+accountHolderCity = "Noida";
+isAccountActive = false;
+
+console.table([accoutNumber,accountHolderEmail,accountPassword,accountHolderCity,isAccountActive]);
