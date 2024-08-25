beb907df390dc51143906df91baea171c87d6619
Nitish-project-repo <nitish.projects.repo@gmail.com>
2024-08-25 17:08:33 +0000

    25/08/24 notes

---
+++ 01_basics/06_string.js
@@ -0,0 +1,47 @@
+const userName = "Xeko";  // string declaration
+//another way of string declaration
+const userLastName = new String("XaXa");
+
+const repoCount = 50;
+
+// using concatination display the both
+
+// console.log("Full Name : " + userName + " " + userLastName);
+// console.log("String has "+ repoCount + " repo count in JS.");
+
+// String interpolation : readability
+console.log(`String has ${repoCount} repo count in JS.`);
+
+/* 
+    > String Methods : 
+        > length
+        > toUpperCase()
+        > toLowerCase()
+
+        > charAt()
+        > at()
+
+        > split()
+        > toString()
+        > concat()
+
+        ++++++++++++  creating substring
+        > slice()
+        > substr()
+        > substring()
+
+        > trim()
+
+        > replace()
+        > replaceAll() 
+
+        > includes()
+
+        > startsWith()
+        > endswith()
+
+        +++++++++++++  finds char/string
+        > indexOf()
+        > lastIndexOf()
+        > find()
+*/
