31bc9309aa94930d968e09e27566de41c1fc7a25
Nitish-project-repo <nitish.projects.repo@gmail.com>
2024-08-25 17:49:10 +0000

    notes on string

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -45,3 +45,12 @@ console.log(`String has ${repoCount} repo count in JS.`);
         > lastIndexOf()
         > find()
 */
+
+let site_url = "https://ideal-doodle-q79rx6r7ggggh4w55.github.dev/";
+
+// console.log(`Length of the site url : ${site_url}`);
+// console.log(`Site url in upper case : ${site_url.toUpperCase()}`);
+// console.log(`Site name : ${site_url.split(".")[1]}`);
+console.log(site_url.substr(site_url.split(".")[0].indexOf("i"),5));
+
+console.log(site_url.find)
