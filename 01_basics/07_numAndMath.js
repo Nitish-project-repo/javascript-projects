5794a025afbb182fd1f840739aa2ffde82d23c44
Nitish-project-repo <nitish.projects.repo@gmail.com>
2024-08-26 10:11:01 +0000

    usage of math module

--- 01_basics/07_numAndMath.js
+++ 01_basics/07_numAndMath.js
@@ -16,8 +16,14 @@ let totalStudents = 33;
 
 let avgMarks = new Number(totalMarks/totalStudents);
 
-console.log(avgMarks);                // [Number: 16.6666666666668]
-console.log(avgMarks.toFixed(2), typeof avgMarks.toFixed(2));   //string  // 16.67 : fixing the decimal place to 2 decimal places; also returns string value
-console.log(avgMarks.toPrecision(5), typeof avgMarks.toPrecision(5)); // string // 16.667 : returns string value to 5 significant digits
+// console.log(avgMarks);                // [Number: 16.6666666666668]
+// console.log(avgMarks.toFixed(2), typeof avgMarks.toFixed(2));   //string  // 16.67 : fixing the decimal place to 2 decimal places; also returns string value
+// console.log(avgMarks.toPrecision(5), typeof avgMarks.toPrecision(5)); // string // 16.667 : returns string value to 5 significant digits
 
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+let max = 6;
+let min = 1;
+let diceValue = Math.random() * (max - min + 1);
+console.log( Math.floor(diceValue) + min);
