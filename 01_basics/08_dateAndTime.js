c001abefc0f324c96acac7eb0617570e2ec553e1
Nitish-project-repo <nitish.projects.repo@gmail.com>
2024-09-03 12:43:54 +0000

    date object and time

---
+++ 01_basics/08_dateAndTime.js
@@ -0,0 +1,15 @@
+/**
+ * Date and Time object
+ */
+
+const todayDate = new Date();
+
+console.log(todayDate);
+console.log(todayDate.toDateString());
+console.log(todayDate.toString());
+console.log(todayDate.toLocaleDateString());
+console.log(todayDate.toISOString());
+console.log(todayDate.toJSON());
+console.log(todayDate.toLocaleString());
+console.log(todayDate.toLocaleTimeString("IN"));
+console.log(todayDate.getUTCDate());
