fdc6e1e29837992f1b3ec23fcbfc0521cc7a9215
Nitish-project-repo <nitish.projects.repo@gmail.com>
2024-09-13 07:10:48 +0000

    iife function

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+/* 
+    > Immediately Invoked function
+        > Those which are invoked or called as soon as an application is run.
+        > It is used when a function is needs to be run as the app runs without being polluted(affected) by the global scope.
+        > Following is how we define IIFE: 
+*/
+
+(function serverConnection() {
+    console.log("Connection successful");
+})();
+
+// These function are usually written in a separate file. 
+
+// (function_code)(arguments) ;
