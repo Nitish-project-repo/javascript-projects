5c07ae4d992d2ef35e65b182463a20b07c435043
Nitish-project-repo <nitish.projects.repo@gmail.com>
2024-09-13 07:20:01 +0000

    iife part 3

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -4,11 +4,23 @@
         > It is used when a function is needs to be run as the app runs without being polluted(affected) by the global scope.
         > Following is how we define IIFE: 
 */
-
+// following is a named iife
 (function serverConnection() {
     console.log("Connection successful");
 })();
 
 // These function are usually written in a separate file. 
 
-// (function_code)() ;
+// (function_code)(); using (;) to distinguish b/w 2 or more iife if written together.
+
+// simple iife is usually unnamed function or arrow function
+
+(() => {
+    console.log(`DB is connected`)
+})();
+
+// if argument is to be passed in the simple iife
+
+((name) => {
+    console.log(`Hi ${name}, DB is connected.`)
+})("Nitish");
