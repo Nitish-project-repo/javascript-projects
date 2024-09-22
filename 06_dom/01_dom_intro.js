efa6d95549cf77b848feb541ded802c8be86979b
Nitish-project-repo <nitish.projects.repo@gmail.com>
2024-09-22 08:58:55 +0000

    dom intro

---
+++ 06_dom/01_dom_intro.js
@@ -0,0 +1,10 @@
+/* 
+    DOM : Document Object Model
+    It is the document-object's model structure represented in a tree structure.
+        > Window node is the root.
+        > Child node -> document node -> html node -> head node & body node
+        > head node and body node have there own child node
+
+    Node represents the parent and child objects and its properties in the window object 
+        which represents the stucture of the document of the website.
+*/
